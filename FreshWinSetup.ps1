ed17b4f50c198fa13705981df0be92e9ca573524
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-12 23:13:44 +0530

    Update FreshWinSetup.ps1

--- FreshWinSetup.ps1
+++ FreshWinSetup.ps1
@@ -4,7 +4,7 @@
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install git firefox microsoft-windows-terminal processhacker nmap sublimetext4 python openjdk Office365ProPlus -y
+choco install git firefox microsoft-windows-terminal processhacker nmap sublimetext4 vscode python openjdk Office365ProPlus -y
 curl https://raw.githubusercontent.com/anantkaul/WinTricks/main/Office365/Office365-Activation.cmd -o C:\ProgramData\Office365-Activation.cmd
 C:\ProgramData\Office365-Activation.cmd 
 rm C:\ProgramData\Office365-Activation.cmd
