1304ca6788e275685a231690718614b85a18b05f
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-12 23:16:13 +0530

    Update FreshWinSetup.ps1

--- FreshWinSetup.ps1
+++ FreshWinSetup.ps1
@@ -1,10 +1,18 @@
 # PS
 # Created by @anantkaul
 
+# Setting up chocolatey for Windows ...
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 Set-ExecutionPolicy AllSigned
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install git firefox microsoft-windows-terminal processhacker nmap sublimetext4 vscode python openjdk Office365ProPlus -y
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Customize your softwares here ...
+choco install git firefox microsoft-windows-terminal processhacker nmap sublimetext4 vscode python openjdk -y
+
+# Microsoft Office 365 Installation & Activation ...
+choco install Office365ProPlus -y
 curl https://raw.githubusercontent.com/anantkaul/WinTricks/main/Office365/Office365-Activation.cmd -o C:\ProgramData\Office365-Activation.cmd
 C:\ProgramData\Office365-Activation.cmd 
 rm C:\ProgramData\Office365-Activation.cmd
