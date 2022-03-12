bba8ea21a58d09b24b6c3eea3d7c01b920e3a2c7
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-03-12 23:16:40 +0530

    Update FreshWinSetup.ps1

--- FreshWinSetup.ps1
+++ FreshWinSetup.ps1
@@ -1,14 +1,14 @@
 # PS
 # Created by @anantkaul
 
-# Setting up chocolatey for Windows ...
+# Setting up Chocolatey for Windows ...
 [Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'
 Set-ExecutionPolicy AllSigned
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Customize your softwares here ...
+# Customize your Softwares here ...
 choco install git firefox microsoft-windows-terminal processhacker nmap sublimetext4 vscode python openjdk -y
 
 # Microsoft Office 365 Installation & Activation ...
