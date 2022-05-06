a32f22fa4b527781ee09a8bcf4199029eb279fa3
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-05-07 04:35:50 +0530

    Update setup-smb-service.sh

--- systemd-services/setup-smb-service.sh
+++ systemd-services/setup-smb-service.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-
+echo
+echo \>\> Installing Impacket (smbserver.py) with pip3 ...
+sudo pip3 install impacket
+echo
 echo \>\> Setting up smbserver as systemd service ...
-
-# [ ! -d "/opt/smb-files" ] && sudo mkdir /opt/smb-files
 if [ ! -d "/opt/smb-files" ] 
 then
     sudo mkdir /opt/smb-files
@@ -10,5 +11,5 @@ fi
 sudo curl https://raw.githubusercontent.com/anantkaul/WinTricks/main/systemd-services/smbserver.service -o /etc/systemd/system/smbserver.service
 sudo systemctl enable smbserver
 sudo systemctl start smbserver
-
+echo
 echo \>\> Done setting up smbserver as systemd service ...
