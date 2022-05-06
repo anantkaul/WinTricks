0d795928f1a0d2b4ad1585995b59ff204583a7c6
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-05-07 04:03:08 +0530

    Update setup-smb-service.sh

--- systemd-services/setup-smb-service.sh
+++ systemd-services/setup-smb-service.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-echo -e " >> Setting up smbserver as systemd service ..."
+echo  >> Setting up smbserver as systemd service ...
 
 sudo mkdir /opt/smb-files
 sudo curl https://raw.githubusercontent.com/anantkaul/WinTricks/main/systemd-services/smbserver.service) -o /etc/systemd/system/smbserver.service
 sudo systemctl enable smbserver
 sudo systemctl start smbserver
 
-echo -e " >> Done setting up smbserver as systemd service ..."
+echo  >> Done setting up smbserver as systemd service ...
