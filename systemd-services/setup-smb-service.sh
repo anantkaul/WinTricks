0c4b4c3c422050e6d6a1669ac0ea888f2795be33
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-05-07 04:42:24 +0530

    Update setup-smb-service.sh

--- systemd-services/setup-smb-service.sh
+++ systemd-services/setup-smb-service.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
 echo
+echo \>\> Updating repositories ...
+echo
+sudo apt update
+echo
+echo \>\> Installing Python3 & Pip3 ...
+echo
+sudo apt install python3 python3-pip -y
+echo
 echo \>\> Installing Impacket (smbserver.py) with pip3 ...
+echo
 sudo pip3 install impacket
 echo
 echo \>\> Setting up smbserver as systemd service ...
@@ -13,3 +22,4 @@ sudo systemctl enable smbserver
 sudo systemctl start smbserver
 echo
 echo \>\> Done setting up smbserver as systemd service ...
+echo
